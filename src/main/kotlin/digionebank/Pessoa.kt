bee55716c9518f54a5d1b9451f16e5ed2359003a
HenriqueAzT <henriqueazara3311@gmail.com>
2022-07-14 09:53:47 -0300

    Updated Pessoa and Cliente to abstract

--- src/main/kotlin/digionebank/Pessoa.kt
+++ src/main/kotlin/digionebank/Pessoa.kt
@@ -1,17 +1,6 @@
 package digionebank
 
-class Pessoa {
-    var nome:String = "Henrique"
-    var cpf:String = "878.745.846-78"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val Henrique = Pessoa()
-
-    println(Henrique.pessoaInfo())
-}
+abstract class Pessoa (
+    open val nome:String,
+    open val cpf:String
+)
